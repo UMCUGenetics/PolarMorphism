0c7da5964096fec2d1c26296bbe537804a5d4c22
Joanna von Berg <j.vonberg-24@umcutrecht.nl>
2020-11-23 14:45:00 +0100

    small changes

--- R/prepare.input.R
+++ R/prepare.input.R
@@ -1,26 +1,42 @@
-
 #' AlleleFlip
 #' 
-#' Alleleflip() joins a tibble with summary statistics and a tibble with SNP meta information, by a common SNP id column, and aligns reference and alternative alleles to the meta tibble.
-#' @param sumstats a tibble, with at least columns snpid, a1, a2, beta, se, freq
-#' @param snps a tibble, with at least columns snpid, a1, a2
-#' @param snpid either a character indicating the common column name or, in the case of two differently named columns, a named character vector with the name of the common column in the first tibble as a name
+#' Alleleflip joins a tibble with summary statistics and a tibble with SNP meta information, 
+#' by a common SNP id column, and aligns reference and alternative alleles to the meta tibble.
+#' @param sumstats A tibble, with at least columns snpid, a1, a2, beta, se, freq.
+#' @param snps A tibble, with at least columns snpid, a1, a2.
+#' @param snpid Either a character indicating the common column name or, 
+#' in the case of two differently named columns, 
+#' a named character vector with the name of the common column in the first tibble as a name.
 #' @export
 AlleleFlip <- function(sumstats, snps, snpid = "snpid"){
-  df <- snps %>%
-    inner_join(df, by = by, suffix = c("", ".b"))
-  df$a1.b <- toupper(df$a1.b); df$a2.b <- toupper(df$a2.b)
-  if(identical(df$a1, df$a1.b)){
-    df <- df %>%
-      select(-ends_with(".b")) %>%
-      mutate(z = beta/se)
+  sumstats <- snps %>%
+    dplyr::inner_join(sumstats, by = snpid, suffix = c("", ".b"))
+  sumstats$a1.b <- toupper(sumstats$a1.b); sumstats$a2.b <- toupper(sumstats$a2.b)
+  if(identical(sumstats$a1, sumstats$a1.b)){
+    sumstats <- sumstats %>%
+      dplyr::select(-ends_with(".b")) %>%
+      dplyr::mutate(z = beta/se)
   }else{
-    same <- df$a1 == df$a1.b
-    flip <- df$a1 == df$a2.b
-    df$freq <- ifelse(flip, 1 - df$freq, df$freq)
-    df$beta <- ifelse(flip, -df$beta, df$beta)
-    df <- df %>% mutate(z = beta/se)
-    df$z[!same & !flip] <- NA
+    same <- sumstats$a1 == sumstats$a1.b
+    flip <- sumstats$a1 == sumstats$a2.b
+    sumstats$freq <- ifelse(flip, 1 - sumstats$freq, sumstats$freq)
+    sumstats$beta <- ifelse(flip, -sumstats$beta, sumstats$beta)
+    sumstats$z <- sumstats$beta/sumstats$se
+    sumstats$z[!same & !flip] <- NA
   }
-  return(df)
+  return(sumstats)
 }
+
+#' Import SNPs
+#' 
+#' Imports a tibble with SNP meta information, and subsets to single nucleotide variants.
+#' @param file A path for the SNP-file.
+#' @param a1,a2 The name of the column for the reference and alternative alleles.
+#' @export
+ImportSNPs <- function(file = "~/3.Athero-integration/mtag/mafgt0.1.infogt0.95.ngt10000.freqSElt0.05.snps", a1 = "a1", a2 = "a2"){
+  snps <- readr::read_table2(file, col_names = T)
+  bases <- c("A", "C", "G", "T")
+  snps <- snps %>%
+    dplyr::filter(dplyr::pull(snps, var = a1) %in% bases & dplyr::pull(snps, var = a2) %in% bases)
+}
+
