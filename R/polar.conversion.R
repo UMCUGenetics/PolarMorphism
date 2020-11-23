d53d9db4c3e8fdff38820dcc77e23b5e1107e1f1
Joanna von Berg <j.vonberg-24@umcutrecht.nl>
2020-11-23 15:19:16 +0100

    added function PolarCoords for polar conversion

--- R/polar.conversion.R
+++ R/polar.conversion.R
@@ -1,14 +1,15 @@
 #' ConvertToPolar
 #' 
 #' ConvertToPolar joins two tibbles with summary statistics by a common SNP id column,
-#' with alleles already harmonized, for instance with AlleleFlip.
+#' with alleles already harmonized, for instance with AlleleFlip. 
+#' It then converts the original z-scores for trait 1 and 2 to polar coordinates, describing the angle theta with the x-axis (trait 1), and the distance r.
 #' @param df1 @param df2 tibble, with at least columns snpid, a1, a2, beta, se, freq, pval
 #' @param snpid Either a character indicating the common column name or, 
 #' in the case of two differently named columns, 
 #' a named character vector with the name of the common column in the first tibble as a name.
 #' @export
 ConvertToPolar <- function(df1, df2, snpid){
-  df <- inner_join(df1, df2, by = snpid, suffix = c(".1", ".2"))
+  df <- dplyr::inner_join(df1, df2, by = snpid, suffix = c(".1", ".2"))
   df <- df[complete.cases(df),]
   rm(df1)
   rm(df2)
@@ -17,9 +18,9 @@ ConvertToPolar <- function(df1, df2, snpid){
     dplyr::select(starts_with("z")) %>%
     as.matrix() -> zmat
   zmat.white <- whiten(X = zmat, center = F, method = "ZCA")
-  pol <- CartToPol(x = zmat[,1], y = zmat[,2])
-  polw <- CartToPol(x = zmat.white[,1], y = zmat.white[,2])
-  res <- as_tibble(cbind(polw$r, pol$theta))
+  pol <- PolarCoords(x = zmat[,1], y = zmat[,2])
+  polw <- PolarCoords(x = zmat.white[,1], y = zmat.white[,2])
+  res <- dplyr::as_tibble(cbind(polw$r, pol$theta))
   rm(pol)
   rm(polw)
   colnames(res) <- c("r", "theta")
@@ -36,4 +37,13 @@ ConvertToPolar <- function(df1, df2, snpid){
     mutate(se.1 = df$se.1) %>%
     mutate(se.2 = df$se.2)
   return(res)
+}
+
+#' PolarCoords
+#' 
+#' Given two vectors for x- and y-coordinates, performs polar transformation. Returns the angle theta as a number between -pi and +pi.
+#' @param x @param y vector of coordinates
+PolarCoords <- function(x, y){
+  theta <- atan2(y = y, x = x)%%(2*pi)
+  return(list(r = sqrt(x^2+y^2), theta = theta))
 }
