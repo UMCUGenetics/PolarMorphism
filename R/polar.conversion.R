3ecd99dc8b1cffca2fde030cb1838629e5533074
Joanna von Berg <j.vonberg-24@umcutrecht.nl>
2020-11-23 15:44:56 +0100

    added z

--- R/polar.conversion.R
+++ R/polar.conversion.R
@@ -1,11 +1,11 @@
 #' ConvertToPolar
-#' 
+#'
 #' ConvertToPolar joins two tibbles with summary statistics by a common SNP id column,
-#' with alleles already harmonized, for instance with AlleleFlip. 
+#' with alleles already harmonized, for instance with AlleleFlip.
 #' It then converts the original z-scores for trait 1 and 2 to polar coordinates, describing the angle theta with the x-axis (trait 1), and the distance r.
 #' @param df1 @param df2 tibble, with at least columns snpid, a1, a2, beta, se, freq, pval
-#' @param snpid Either a character indicating the common column name or, 
-#' in the case of two differently named columns, 
+#' @param snpid Either a character indicating the common column name or,
+#' in the case of two differently named columns,
 #' a named character vector with the name of the common column in the first tibble as a name.
 #' @export
 ConvertToPolar <- function(df1, df2, snpid){
@@ -25,22 +25,24 @@ ConvertToPolar <- function(df1, df2, snpid){
   rm(polw)
   colnames(res) <- c("r", "theta")
   res <- res %>%
-    mutate(snpid = df$snpid) %>%
-    mutate(theta = theta%%(2*pi))  %>%
-    mutate(fold = abs(abs(theta - pi) - (0.5*pi))) %>%
-    mutate(traitspecificity = (fold - 0.25*pi)/0.25*pi) %>%
-    mutate(snpid = df$snpid) %>%
-    mutate(pval.1 = df$pval.1) %>%
-    mutate(pval.2 = df$pval.2) %>%
-    mutate(beta.1 = df$beta.1) %>%
-    mutate(beta.2 = df$beta.2) %>% 
-    mutate(se.1 = df$se.1) %>%
-    mutate(se.2 = df$se.2)
+    dplyr::mutate(snpid = df$snpid) %>%
+    dplyr::mutate(theta = theta%%(2*pi))  %>%
+    dplyr::mutate(fold = abs(abs(theta - pi) - (0.5*pi))) %>%
+    dplyr::mutate(traitspecificity = (fold - 0.25*pi)/0.25*pi) %>%
+    dplyr::mutate(snpid = df$snpid) %>%
+    dplyr::mutate(pval.1 = df$pval.1) %>%
+    dplyr::mutate(pval.2 = df$pval.2) %>%
+    dplyr::mutate(beta.1 = df$beta.1) %>%
+    dplyr::mutate(beta.2 = df$beta.2) %>%
+    dplyr::mutate(se.1 = df$se.1) %>%
+    dplyr::mutate(se.2 = df$se.2) %>%
+    dplyr::mutate(z.1 = df$z.1) %>%
+    dplyr::mutate(z.2 = df$z.2)
   return(res)
 }
 
 #' PolarCoords
-#' 
+#'
 #' Given two vectors for x- and y-coordinates, performs polar transformation. Returns the angle theta as a number between -pi and +pi.
 #' @param x @param y vector of coordinates
 PolarCoords <- function(x, y){
