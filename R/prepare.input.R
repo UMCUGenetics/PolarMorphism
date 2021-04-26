f08c0ce208437e96b42578a741de68340f68e4e3
Joanna von Berg <j.vonberg-24@umcutrecht.nl>
2021-04-26 10:58:46 +0200

    added option for only one allele column

--- R/prepare.input.R
+++ R/prepare.input.R
@@ -1,25 +1,26 @@
 #' AlleleFlip
-#' 
-#' Alleleflip joins a tibble with summary statistics and a tibble with SNP meta information, 
+#'
+#' Alleleflip joins a tibble with summary statistics and a tibble with SNP meta information,
 #' by a common SNP id column, and aligns reference and alternative alleles to the meta tibble.
-#' @param sumstats A tibble, with at least columns snpid, a1, a2, beta, se, freq.
+#' @param sumstats A tibble, with at least columns snpid, a1, a2, beta, se, freq, pval.
 #' @param snps A tibble, with at least columns snpid, a1, a2.
-#' @param snpid Either a character indicating the common column name or, 
-#' in the case of two differently named columns, 
+#' @param snpid Either a character indicating the common column name or,
+#' in the case of two differently named columns,
 #' a named character vector with the name of the common column in the first tibble as a name.
 #' @export
-AlleleFlip <- function(sumstats, snps, snpid = "snpid"){
+AlleleFlip <- function(sumstats, snps, snpid = "snpid", only.a2 = F){
   sumstats <- snps %>%
     dplyr::inner_join(sumstats, by = snpid, suffix = c("", ".b"))
-  sumstats$a1.b <- toupper(sumstats$a1.b); sumstats$a2.b <- toupper(sumstats$a2.b)
-  if(identical(sumstats$a1, sumstats$a1.b)){
+  if(!only.a2){sumstats$a1.b <- toupper(sumstats$a1.b)}
+  sumstats$a2.b <- toupper(sumstats$a2.b)
+  if(identical(sumstats$a2, sumstats$a2.b)){
     sumstats <- sumstats %>%
       dplyr::select(-ends_with(".b")) %>%
       dplyr::mutate(z = beta/se)
   }else{
-    same <- sumstats$a1 == sumstats$a1.b
-    flip <- sumstats$a1 == sumstats$a2.b
-    sumstats$freq <- ifelse(flip, 1 - sumstats$freq, sumstats$freq)
+    same <- sumstats$a2 == sumstats$a2.b
+    if(only.a2){flip <- sumstats$a2 != sumstats$a2.b}else{flip <- sumstats$a2 == sumstats$a1.b}
+    if(!only.a2){sumstats$freq <- ifelse(flip, 1 - sumstats$freq, sumstats$freq)}
     sumstats$beta <- ifelse(flip, -sumstats$beta, sumstats$beta)
     sumstats$z <- sumstats$beta/sumstats$se
     sumstats$z[!same & !flip] <- NA
@@ -28,7 +29,7 @@ AlleleFlip <- function(sumstats, snps, snpid = "snpid"){
 }
 
 #' Import SNPs
-#' 
+#'
 #' Imports a tibble with SNP meta information, and subsets to single nucleotide variants.
 #' @param file A path for the SNP-file.
 #' @param a1,a2 The name of the column for the reference and alternative alleles.
