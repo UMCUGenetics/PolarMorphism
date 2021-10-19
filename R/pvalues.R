657353ab976ccacee71dd5d13f855d7c7a7e7906
Joanna von Berg <j.vonberg-24@umcutrecht.nl>
2021-10-19 11:35:21 +0200

    Added documentation, changed some details

--- R/pvalues.R
+++ R/pvalues.R
@@ -11,6 +11,7 @@ PvalueForR <- function(r, p = 2, exp.r = NA){
   return(chi.pval)
 }
 
+# TermFuncKappa is the modified bessel function of order i for the given kappa, where q is the angle to be tested, multiplied by sin(i*q)/i
 GetTermFunc <- function(kappa){
   result <- function(q, i){
     result2 <- (besselI(x = kappa, nu = i, expon.scaled = FALSE) * sin(i * q))/i
@@ -20,21 +21,30 @@ GetTermFunc <- function(kappa){
 }
 
 pvalangle <- function(relative.angle, kappa, tol = 1e-20, stepsize = 10){
-  relative.angle <- relative.angle %% (2*pi)
+  # We need this function for the infinite sum that approaches the integral of the probability density function.
   TermFuncKappa <- GetTermFunc(kappa = kappa)
+  # We initialize a vector of the same length as relative.angle, to store the sum results in
   sum <- rep(0, length(relative.angle))
   flag <- TRUE
+  # We don't add 1 term but *stepsize* number of terms at a time to save compute time
   iters <- 1:stepsize
   while(flag){
+    # terms is a stepsize x m matrix, where m is the number of SNPs (length of relative.angle)
     terms <- mapply(FUN = TermFuncKappa, relative.angle, MoreArgs = list(iters))
+    # we sum each column of terms to get the sum per SNP
     sum <- sum + apply(X = terms, MARGIN = 2, FUN = sum)
-    if(all(abs(terms[nrow(terms),]) < tol)){
+    # we continue summing until the last term that we calculated is so small that it doesn't make much of a difference
+    # we now wait until this is true for each SNP, maybe it could save time to only go on calculating for the SNPs that need it
+    # but if we need to make new matrices each time that happens, it's probably not faster
+    if(all(abs(terms[nrow(terms),]) < tol, na.rm = T)){
       flag <- FALSE
     }else{
+      # we update the vector iters so it will calculate the *stepsize* next steps
       iters <- iters + stepsize
     }
   }
-  res <- (relative.angle/(2*pi) + sum/(pi * besselI(x = kappa, nu = 0, expon.scaled = FALSE)) - 0.5) * 2
+  # Once we have the sums, we do the calculation below to get the approximate value of the integral of the probability function for each SNP (i.e. the p-values)
+  res <- ((relative.angle/(2*pi)) + sum/(pi * besselI(x = kappa, nu = 0, expon.scaled = FALSE)) + 0.5) * 2
   res[res == -1] <- 0
   return(res)
 }
@@ -43,9 +53,10 @@ pvalangle <- function(relative.angle, kappa, tol = 1e-20, stepsize = 10){
 #'
 #' PvalueForAngle performs p-value estimation using the von Mises distribution
 #' @param angle.trans a vector of fourfold transformed angles
-#' @param r a vector of distances, either expected distance (z.max) or actual distance
+#' @param r a vector of distances
 #' @export
 PvalueForAngle <- function(angle.trans, r, tol = 1e-20, kappa.file = "~/git/PolarMorphism/R/kappas.4foldtransform.Rda"){
+  angle.trans <- -abs(angle.trans)
   load(kappa.file)
   kappas.list <- kappas.list2
   angle.pval <- rep(NA, length(angle.trans))
